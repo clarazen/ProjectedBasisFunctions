baf544d8bf142dfde4f7f58c7bc0e1dd555395f4
cmmenzen <c.m.menzen@tudelft.nl>
2022-11-03 07:18:32 +0100

    add function to compute error measures

---
+++ errormeasures.jl
@@ -0,0 +1,13 @@
+function errormeasures(mstar::Vector,Pstar::Vector,ytest::Vector,σ_n::Float64)
+
+    # !! Pstar needs to be the variance of the prediction
+    SMSE = 0
+    MSLL = 0
+    for i = 1:length(mstar)
+        SMSE = SMSE + (ytest[i] - mstar[i])^2 / var(ytest)
+        MSLL = MSLL + (ytest[i] - mstar[i])^2 / (Pstar[i] + σ_n^2) + log(2*π*(Pstar + σ_n^2)) / 2length(mstar)
+    end
+
+    return SMSE,MSLL
+end
+
