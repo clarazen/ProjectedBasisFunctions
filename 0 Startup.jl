dcb56ffbfdb237a224b612029617e8ac6c2af354
cmmenzen <c.m.menzen@tudelft.nl>
2023-03-30 09:22:06 +0200

    minor changes

--- 0 Startup.jl
+++ 0 Startup.jl
@@ -4,3 +4,6 @@ import Pkg;
 Pkg.add("Plots")
 Pkg.add("StatsBase")
 Pkg.add("DelimitedFiles")
+
+# download data from https://fdm-fallback.uni-kl.de/TUK/FB/MV/WSKL/0001/Robot_Identification_Benchmark_Without_Raw_Data.rar
+# and save data as "benchmark data/X_kukainv.csv", "benchmark data/y_kukainv.csv", "benchmark data/Xtest_kukainv.csv", "benchmark data/ytest_kukainv.csv"
